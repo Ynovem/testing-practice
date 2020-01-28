46dc237887451569d6c7d81eab26d9bf062a0c3d
Sohar Gabor <soharka3@gmail.com>
2020-01-28 23:35:43 +0100

    Solve centuryFromYear test

--- test/tst_century_from_year_calculator.cpp
+++ test/tst_century_from_year_calculator.cpp
@@ -1,9 +1,14 @@
 #include <catch.hpp>
 
 int centuryFromYear(int year) {
-	return 0;
+	int result = (year - 1) / 100;
+	return result + 1;
 }
 
 TEST_CASE( "centuryFromYear", "first" ) {
 	REQUIRE( centuryFromYear(1) == 1 );
+	REQUIRE( centuryFromYear(100) == 1 );
+	REQUIRE( centuryFromYear(101) == 2 );
+	REQUIRE( centuryFromYear(2000) == 20 );
+	REQUIRE( centuryFromYear(2020) == 21 );
 }

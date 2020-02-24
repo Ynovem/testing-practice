e02f7d5301be724222ab5ad5219159945e83f434
Fehér Gergő <feher.gerg@gmail.com>
2020-02-24 22:00:09 +0100

    cleaning

--- CMakeModules/Catch.cmake
+++
@@ -1,1 +0,0 @@
-include_directories(${CMAKE_SOURCE_DIR}/3rd-party/catch)
--- test/tst_century_from_year_calculator.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch.hpp>
-
-int centuryFromYear(int year) {
-	return 0;
-}
-
-TEST_CASE( "centuryFromYear", "first" ) {
-	REQUIRE( centuryFromYear(1) == 1 );
-}
